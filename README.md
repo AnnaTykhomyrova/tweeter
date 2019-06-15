a3a0eea120d0cb2eef978eb57acdd761ed45bfa8
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-06-15 21:05:01 +0000

    added helper time ago function in app.js and fixed code

--- README.md
+++ README.md
@@ -30,6 +30,7 @@ Tweeter Project is build upon HTML, CSS, JS, jQuery and AJAX front-end skills, a
 ## Getting Started
 
 1. Install dependencies using the `npm install` command.
-2. Go to <http://localhost:8080/> in your browser.
+2. Start the web server using the `npm run local` or `npm start` command. The app will be served at http://localhost:8080/.
+3. Go to <http://localhost:8080/> in your browser.
 
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,37 +2,55 @@
 // JavaScript code
 $(document).ready(function() {
 
-  // function that prevent Cross-Site Scripting
-  function escape(str) {
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
+// function that prevent Cross-Site Scripting
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+return div.innerHTML;
 }
 
-// function that creates new tweets
-  function createTweetElement (tweetObj) {
-    var $tweetArticle = `
-      <article class="tweets-container">
-          <header>
-            <div class="logo2"><img src='${tweetObj.user.avatars.small}'></div>
-            <span class="header">${tweetObj.user.name}</span>
-            <span class="header_el">${tweetObj.user.handle}</span>
-          </header>
-          <section>
-            <h3>${escape(tweetObj.content.text)}</h3>
-          </section>
-          <footer>
-            <h5>${tweetObj.created_at}</h5>
-            <div class="footer_images">
-              <i class="fas fa-flag"></i>
-              <i class="fas fa-retweet"></i>
-              <i class="fas fa-heart"></i>
-            </div>
-          </footer>
-        </article>
-    `;
-    return $tweetArticle;
+//Converts milliseconds to appropriate time for user readability
+const convertTime = (milsec) => {
+  const sec = (Date.now() - milsec ) / 1000;
+  const min = sec / 60;
+  const hour = min / 60;
+  const day = hour / 24;
+
+  if ( min < 60 ) {
+      return `${Math.floor(min)}m ago`;
   }
+  else if ( min > 60 ) {
+      return `${Math.floor(hour)}h ago`;
+  }
+  else if ( hour > 24 ) {
+      return `${Math.floor(day)}days ago`;
+  }
+};
+
+// function that creates new tweets
+function createTweetElement (tweetObj) {
+  var $tweetArticle = `
+    <article class="tweets-container">
+        <header>
+          <div class="logo2"><img src='${tweetObj.user.avatars.small}'></div>
+          <span class="header">${tweetObj.user.name}</span>
+          <span class="header_el">${tweetObj.user.handle}</span>
+        </header>
+        <section>
+          <h3>${escape(tweetObj.content.text)}</h3>
+        </section>
+        <footer>
+          <h5>${convertTime(tweetObj.created_at)}</h5>
+          <div class="footer_images">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+  `;
+  return $tweetArticle;
+}
 
 // function that shows new tweets on the page
 function renderTweets (tweetData) {
@@ -68,24 +86,24 @@ $form.on('submit',function (ev) {
   ev.preventDefault();
   const data = $form.serialize();
   var input = $('#new-tweet-form textarea').val();
-  if ($(".error")) {
-    $(".error").slideUp(true);
-  }
-  if (input.length === 0) {
-    return $(".error").text("Error: Your tweet content is not present").slideDown(true);
-  } else if (input.length > 140) {
-    return $(".error").text("Error: Your tweet content is too long").slideDown(true);
-  } else {
-    $('textarea').val('');
-    $('.counter').text('140');
-    $.ajax({
-      method: 'POST',
-      url: '/tweets/',
-      data: data,
-      success: function () {
-        loadTweets();
-      }
-    });
-  }
-});
+    if ($(".error")) {
+      $(".error").slideUp(true);
+    }
+    if (input.length === 0) {
+      return $(".error").text("Error: Your tweet content is not present").slideDown(true);
+    } else if (input.length > 140) {
+      return $(".error").text("Error: Your tweet content is too long").slideDown(true);
+    } else {
+      $('textarea').val('');
+      $('.counter').text('140');
+      $.ajax({
+        method: 'POST',
+        url: '/tweets/',
+        data: data,
+        success: function () {
+          loadTweets();
+        }
+      });
+    }
+  });
 });
